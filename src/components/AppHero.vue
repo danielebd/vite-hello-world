50771bba244b0f416eaeef9747bc2ac182e9e26f
danielebd <danielebalebalestro@gmail.com>
2023-03-22 17:44:34 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        data (){
+            return {
+                title: 'La mia prima app con Vite!',
+                paragraph: 'duretta eh..',
+                button: 'dai su.. cliccami'
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="container">
+        <h1>{{title}}</h1>
+        <p>{{ paragraph }}</p>
+        <button> {{ button }}</button>
+    </div>
+</template>
+    
+<style scoped>
+div {
+    text-align: center;
+    background-color: rgb(197, 168, 0);
+    border-radius: 20px;
+    padding: 40px;
+}
+
+button {
+    border-radius: 10px;
+    border: 0px;
+    padding: 5px;
+    background-color: blue;
+    color: rgb(197, 168, 0) ;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data (){
+        return {
+            message: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+<template>
+    <h1>{{ message }}</h1>
+</template>
+<style scoped>
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+  color:rgb(41, 15, 65)
+}
+</style>
